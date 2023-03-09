94150f97f2e3126fccee20a3c4a94c8c9ec020e5
dontrellthedeveloper <dontrellknight@gmail.com>
2023-03-08 22:15:45 -0500

    added top charts page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -30,7 +30,7 @@ import {createApi, fakeBaseQuery, fetchBaseQuery} from '@reduxjs/toolkit/query/r
         getSongRelated: builder.query({query: ({songid}) => `/v1/tracks/related?track_id=${songid}`}),
         getArtistDetails: builder.query({query: (artistId) => `/v2/artists/details?artist_id=${artistId}`}),
         getSongsByCountry: builder.query({query: (countryCode) => `/v1/charts/country?country_code=${countryCode}`}),
-
+        // getTopCharts: builder.query({query: (countryCode) => `/v1/charts/country?country_code=${countryCode}`}),
     })
  })   
 
@@ -41,5 +41,4 @@ export const {
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
     useGetSongsByCountryQuery,
-
 } = shazamApi
