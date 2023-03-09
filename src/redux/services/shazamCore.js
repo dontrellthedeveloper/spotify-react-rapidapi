f74566629442c998a66b0299d5384bb2e76dd48a
dontrellthedeveloper <dontrellknight@gmail.com>
2023-03-08 22:51:37 -0500

    added search functionality and added the ability to filter through songs by genre

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -26,11 +26,14 @@ import {createApi, fakeBaseQuery, fetchBaseQuery} from '@reduxjs/toolkit/query/r
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({query: () => '/v1/charts/world'}),
+        getSongsByGenre: builder.query({query: (genre) => `/v1/charts/genre-world?genre_code=${genre}`}),
         getSongDetails: builder.query({query: ({songid}) => `/v1/tracks/details?track_id=${songid}`}),
         getSongRelated: builder.query({query: ({songid}) => `/v1/tracks/related?track_id=${songid}`}),
         getArtistDetails: builder.query({query: (artistId) => `/v2/artists/details?artist_id=${artistId}`}),
         getSongsByCountry: builder.query({query: (countryCode) => `/v1/charts/country?country_code=${countryCode}`}),
-        // getTopCharts: builder.query({query: (countryCode) => `/v1/charts/country?country_code=${countryCode}`}),
+        getSongsBySearch: builder.query({query: (searchTerm) => `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`}),
+        
+  
     })
  })   
 
@@ -41,4 +44,7 @@ export const {
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
     useGetSongsByCountryQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsBySearchQuery
+
 } = shazamApi
