ac1ee9898e427738d91f8f67075d65a5bf2966cd
dontrellthedeveloper <dontrellknight@gmail.com>
2023-03-10 02:03:58 -0500

    forgot to change the mobile styling for sidebar. Now updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,7 +33,7 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#1D1D1D]">
         <img src={logo2} alt="logo" style={{width: '129px'}} className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
@@ -47,7 +47,7 @@ const Sidebar = () => {
         )}
       </div>
 
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] bg-black backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#1D1D1D] bg-black backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo2} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
